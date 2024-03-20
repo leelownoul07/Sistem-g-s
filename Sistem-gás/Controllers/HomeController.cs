558ccfc8e312e18f62da63556aab923b3021a622
Administrador <Administrador@A6050>
2024-03-19 21:53:18 -0300

    Adicionar arquivos de projeto.

---
+++ Sistem-gás/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Sistem_gás.Models;
+using System.Diagnostics;
+
+namespace Sistem_gás.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+       
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Sistem-gás/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Sistem_gás.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
